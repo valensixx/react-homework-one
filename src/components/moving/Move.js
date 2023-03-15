24bf7d8a8a4daddf427ab0416536d54530072a4b
valensixx <valensixx@gmail.com>
2023-03-15 21:27:32 +0200

    add some styles

--- src/components/moving/Move.js
+++ src/components/moving/Move.js
@@ -8,12 +8,11 @@ export default function Move(){
     return(
         <div>
             <div>
-                <button>Up</button>
-                <button>Down</button>
-                <button>Left</button>
-                <button>Right</button>
+                <div style={{marginLeft:'25px'}}><button>Up</button></div>
+                <div><button>Left</button> <button>Right</button></div>
+                <div style={{marginLeft:'15px'}}><button>Down</button></div>
             </div>
-            <div style={{height:'500px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <div style={{height:'300px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
                 <div style={{width:'50px', height: '50px', background:'blueviolet'}}></div>
             </div>
         </div>
