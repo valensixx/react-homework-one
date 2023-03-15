498f4855399638794dd4cbecaeb89ce8a28d4464
valensixx <valensixx@gmail.com>
2023-03-15 19:09:58 +0200

    menu and header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Menu from "./components/menu/Menu";
+import Header from "./components/header/Header";
 //*create web page with components: menu, header, content and footer
 
 export default function App(){
     return (
-        <div></div>
+        <div>
+            <Menu />
+            <Header />
+        </div>
     )
 }
---
+++ src/components/menu/Menu.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Menu(){
+    return (
+        <div>
+            <div style={{heigh: '75px', textAlign: 'center'}}>
+                Menu
+            </div>
+        </div>
+    );
+};
