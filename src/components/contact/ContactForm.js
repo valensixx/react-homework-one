d349b0b3f6df5f563d01c34a761795fa0ec1e9d7
valensixx <valensixx@gmail.com>
2023-03-16 20:45:21 +0200

    contact component done

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -16,19 +16,37 @@ export default function Contact(){
             <form>
                 <div>
                     <label>Name</label>
-                    <input type = "text"></input>
+                    <input type = "text" onInput={(e)=>{
+                        setName(e.target.value);
+                    }}></input>
+                    {name.length < 3 ? 'name is required 3 or more simbols' : ''}
                 </div>
                 <div>
                     <label>Email</label>
-                    <input type = "text"></input>
+                    <input type = "text" onInput={(e)=>{
+                        setEmail(e.target.value);
+                    }}></input>
+                    {email.length < 1 ? 'email is required' : ''}
                 </div>
                 <div>
                     <label>Topic</label>
-                    <input type = "text"></input>
+                    <input type = "text" onInput={(e)=>{
+                        setTopic(e.target.value);
+                    }}></input>
+                    {topic.length < 10 ? 'topic required min 10 simbols' : ''}
                 </div>
                 <div>
-                    <label>Message</label>
-                    <textarea></textarea>
+                    <label >Message</label>
+                    <textarea onInput={(e)=>{
+                        setMessage(e.target.value);
+                    }}></textarea>
+                    {message.length < 1 ? 'message is required' : ''}
+                </div>
+                <div>
+                    {name} <br/>
+                    {email} <br/>
+                    {topic} <br/>
+                    {message}
                 </div>
                 <div>
                     <button>Send</button>
