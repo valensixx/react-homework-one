8a3089d126f1a9240021335c12d8b7aabc71d933
valensixx <valensixx@gmail.com>
2023-03-16 20:04:38 +0200

    consts and html structure

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,7 +1,39 @@
 import React,{useState} from "react";
 
+/*
+create contact form with name( a must input, with at least 3 simbols),
+email(a must input), Topic (a must input - at least 10 simbols),
+message(text area, a must input) and a send button.
+*/
+
 export default function Contact(){
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [topic, setTopic] = useState('');
+    const [message, setMessage] = useState('');
     return(
-        <div></div>
+        <div>
+            <form>
+                <div>
+                    <label>Name</label>
+                    <input type = "text"></input>
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input type = "text"></input>
+                </div>
+                <div>
+                    <label>Topic</label>
+                    <input type = "text"></input>
+                </div>
+                <div>
+                    <label>Message</label>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <button>Send</button>
+                </div>
+            </form>
+        </div>
     );
 };
