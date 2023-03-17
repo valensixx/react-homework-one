419f92082443b81ed496b5bd3b1601c0555daba9
valensixx <valensixx@gmail.com>
2023-03-17 20:06:57 +0200

    css and html/jsx done

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,12 +1,31 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import './Menu.css';
 //create hamburger menu
-export default function Menu(){
+export default function Menu() {
     return (
         <div>
-            <div style={{heigh: '75px', textAlign: 'center'}}>
-                Menu
-            </div>
+            <nav className="menu">
+                <div className="toggle">
+                    <button onClick={() => {
+
+                    }}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                        </svg>
+                    </button>
+                </div>
+                <ul className="menu-items">
+                    <li>
+                        <a href="">Home</a>
+                    </li>
+                    <li>
+                        <a href="">About us</a>
+                    </li>
+                    <li>
+                        <a href="">Contacts</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     );
 };
